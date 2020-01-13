7bc63f3108e325d9c9ebf9f6da9004106f38c785
paka747 <paka747@gmail.com>
2020-01-13 20:37:47 +0100

    Add unpack and pack methods to API

--- app.py
+++ app.py
@@ -8,9 +8,11 @@ from flask_restful import Resource, Api
 from json import dumps
 from flask_jsonpify import jsonify
 from werkzeug.utils import secure_filename
+from program import Parser
 
 UPLOAD_FOLDER = '/path/to/the/uploads'
-ALLOWED_EXTENSIONS = {'bin'}
+ALLOWED_PACK_EXTENSIONS = {'zip'}
+ALLOWED_UNPACK_EXTENSIONS = {'bin'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -18,15 +20,15 @@ api = Api(app)
 
 CORS(app)
 
-def allowed_file(filename):
+def allowed_file(filename, extensions):
     return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+           filename.rsplit('.', 1)[1].lower() in extensions
 
 @app.route('/')
 def home():
     return 'Application works!'
 
-@app.route('/parse_table', methods=['POST'])
+@app.route('/unpackFile', methods=['POST'])
 def unpackFile():
     # check if the post request has the file part
     if 'file' not in request.files:
@@ -38,7 +40,7 @@ def unpackFile():
     if file.filename == '':
         flash('No selected file')
         return redirect(request.url)
-    if file and allowed_file(file.filename):
+    if file and allowed_file(file.filename, ALLOWED_UNPACK_EXTENSIONS):
         inputFileName = secure_filename(file.filename)
         isDirectory = os.path.isdir(inputFileName)
         isFile = os.path.isfile(inputFileName)
@@ -51,13 +53,44 @@ def unpackFile():
             return redirect(request.url)
         _, inputFileExtension = os.path.splitext(inputFileName)
         try:
-            import program
-            if inputFileExtension == '.bin':
-                program.Parser.unpackWatchFace(inputFileName)
-            elif inputFileExtension == '.json':
-                program.Parser.packWatchFace(inputFileName)
-            else:
-                print("The app doesn't support file with extension %s." % (inputFileExtension,))
+            Parser.unpackWatchFace(inputFileName)
+            print("Done")
+        except Exception as e:
+            print('[Fatal] %s' % (e,))
+            import traceback
+            traceback.print_stack()
+            logging.exception(e)
+        # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        # return redirect(url_for('uploaded_file',
+        #                         filename=filename))
+    return""
+
+@app.route('/packFile', methods=['POST'])
+def packFile():
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    # if user does not select file, browser also
+    # submit an empty part without filename
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if file and allowed_file(file.filename, ALLOWED_PACK_EXTENSIONS):
+        inputFileName = secure_filename(file.filename)
+        isDirectory = os.path.isdir(inputFileName)
+        isFile = os.path.isfile(inputFileName)
+        if not isDirectory and not isFile:
+            print("File or direcotry %s doesn't exists." % (inputFileName,))
+            flash("File or direcotry %s doesn't exists." % (inputFileName,))
+            return redirect(request.url)
+        if isDirectory:
+            flash('Not supported yet')
+            return redirect(request.url)
+        _, inputFileExtension = os.path.splitext(inputFileName)
+        try:
+            Parser.unpackWatchFace(inputFileName)
             print("Done")
         except Exception as e:
             print('[Fatal] %s' % (e,))
